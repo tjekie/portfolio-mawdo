c2f7953d676518a5358a88a51609034107e2b093
tjekie <sonfirst250@gmail.com>
2024-07-25 23:54:05 -0300

    Add files via upload

---
+++ personelwebsite/spripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Bedankt voor je bericht, we nemen spoedig contact met je op!');
+});
